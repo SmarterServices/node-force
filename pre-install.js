05034c10e085f126565c636179449a71d4bc6023
Tanzir Ul Islam <tanzir.islam@enosisbd.com>
2016-10-10 18:10:26 +0600

    Pre install postgres instruction in package.json

---
+++ pre-install.js
@@ -0,0 +1,36 @@
+var shelljs = require('shelljs');
+var os = require('os');
+
+/*
+ What platform you're running on: 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+ win32 (for 32 or 64 bit)
+ */
+
+var isWin = /^win32/.test(os.platform());
+var isLinux = /^linux/.test(os.platform());
+var isMac = /^darwin/.test(os.platform()) || /^freebsd/.test(os.platform());
+
+shelljs.echo('*************************** pre install postgres!!! ************************************');
+console.log('*************************** pre install postgres!!! ************************************');
+
+if (isLinux) {
+  // For REDHAT LINUX
+/*    if (shelljs.exec('sudo yum install postgresql-devel').code !== 0) {
+    shelljs.echo('Error in install libpq-dev!!!');
+    shelljs.exit(1);
+  }*/
+
+  //For UBUNTU
+   if (shelljs.exec('sudo apt-get install libpq-dev').code !== 0) {
+     shelljs.echo('Error in install libpq-dev!!!');
+     shelljs.exit(1);
+   }
+} else if (isMac) {
+  if (shelljs.exec('brew install postgres').code !== 0) {
+    shelljs.echo('Error in install postgres!!!');
+    shelljs.exit(1);
+  }
+} else {
+  //for other os, no support right now
+  console.log('for other os, no support right now');
+}
