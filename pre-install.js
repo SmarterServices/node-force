1958834da31974bb746a9e98e407d5f35ea70a71
Tanzir Ul Islam <tanzir.islam@enosisbd.com>
2016-10-12 14:24:07 +0600

    #132143049 - Include postgres build instruction in npm install

--- pre-install.js
+++ pre-install.js
@@ -11,26 +11,39 @@ var isLinux = /^linux/.test(os.platform());
 var isMac = /^darwin/.test(os.platform()) || /^freebsd/.test(os.platform());
 
 shelljs.echo('*************************** pre install postgres!!! ************************************');
-console.log('*************************** pre install postgres!!! ************************************');
 
 if (isLinux) {
   // For REDHAT LINUX
-/*    if (shelljs.exec('sudo yum install postgresql-devel').code !== 0) {
+/*  if (shelljs.exec('sudo yum install postgresql-devel').code !== 0) {
     shelljs.echo('Error in install libpq-dev!!!');
     shelljs.exit(1);
+  } else {
+    shelljs.exec('npm install pg');
+    shelljs.exec('npm install pg-native');
+    shelljs.exec('npm install sequelize');
   }*/
-
   //For UBUNTU
    if (shelljs.exec('sudo apt-get install libpq-dev').code !== 0) {
      shelljs.echo('Error in install libpq-dev!!!');
      shelljs.exit(1);
+   } else {
+     shelljs.exec('npm install pg');
+     shelljs.exec('npm install pg-native');
+     shelljs.exec('npm install sequelize');
    }
 } else if (isMac) {
   if (shelljs.exec('brew install postgres').code !== 0) {
     shelljs.echo('Error in install postgres!!!');
     shelljs.exit(1);
+  } else {
+    shelljs.exec('npm install pg');
+    shelljs.exec('npm install pg-native');
+    shelljs.exec('npm install sequelize');
   }
 } else {
   //for other os, no support right now
-  console.log('for other os, no support right now');
+  console.log('for other os, no support right now! Error for postgres db may occur. Please install postgres db manually if error occurs!');
+  shelljs.exec('npm install pg');
+  shelljs.exec('npm install pg-native');
+  shelljs.exec('npm install sequelize');
 }
